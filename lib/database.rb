40f2a4e8258d0f5ff27ff85a16932d89850a0edf
Ian Vaughan <github@ianvaughan.co.uk>
2014-01-03 18:00:46 +0000

    Dont recreate table, database config in file, and save extra info for likes for now

--- lib/database.rb
+++ lib/database.rb
@@ -4,29 +4,38 @@ class Database
 
   def initialize
     connect
-    create_table
+    # create_table
   end
 
   def connect
+    config = YAML.load_file('./config/database.yml')
     @db = Sequel.connect(
-      "postgres://user:password@host:port/database_name",
-      :max_connections => 10,
-      :logger => Logger.new('log/db.log'))
+      "postgres://#{config[:user]}:#{config[:password]}@#{config[:host]}:#{config[:port]}/#{config[:database_name]}",
+      :max_connections => config[:max_connections],
+      :logger => Logger.new(config[:log])
+    )
   end
 
   def create_table
+    @db.drop_table :likes
     @db.create_table :likes do
       primary_key :id
-      Integer :user_id
-      Integer :id
+      String :user_id
+      String :id
+      String :category
+      String :name
     end
   end
 
   def dataset
-    @dataset ||= @db[:items]
+    @dataset ||= @db[:likes]
   end
 
-  def add user, item
-    dataset.insert(user_id: user.id, id: item.id)
+  def add user_id, item_id, category, name
+    dataset.insert(user_id: user_id, id: item_id, category: category, name: name)
+  end
+
+  def get_all_for user_id
+    dataset.filter(user_id: user_id)
   end
 end
