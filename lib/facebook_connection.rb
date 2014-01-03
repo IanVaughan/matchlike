d3f366df99ac36196fdcc04434ef7a7a5e23edfd
Ian Vaughan <github@ianvaughan.co.uk>
2014-01-03 17:10:45 +0000

    Read config from config file

--- lib/facebook_connection.rb
+++ lib/facebook_connection.rb
@@ -1,15 +1,19 @@
 class FacebookConnection
+  attr_accessor :app_id, :app_secret, :scopes
 
-  def initialize(oauth_access_token)
-    # app_id, app_secret, callback_url = 1435770329973305, '027a9b991a6d50d846ac14256e738b65', nil
+  def initialize
+    config ||= YAML.load_file('./config/config.yml')
+    @app_id = config[:id]
+    @app_secret = config[:secret]
+    @scopes = config[:scopes].join(',')
+    @callback_url = nil
+  end
 
-    # oauth = Koala::Facebook::OAuth.new(app_id, app_secret, callback_url)
-
-    # oauth_access_token = oauth.get_app_access_token
+  def auth(oauth_access_token)
     @graph = Koala::Facebook::API.new(oauth_access_token)
   end
 
   def get_likes
-    @graph.get_connections("me", "likes", limit:1000)
+    @graph.get_connections('me', 'likes', limit:1000)
   end
 end
