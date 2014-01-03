7c683801e510aef06cf4501ee49032c17ce172d1
Ian Vaughan <github@ianvaughan.co.uk>
2014-01-03 18:09:17 +0000

    Use my classes
    Use single quotes
    Remote extra post '/', not sure what its fore
    Rename fb_error to flash - still to check if real flash exists
    Save likes in DB - still to work out when to update and delete
    Tidy up main view

--- lib/server.rb
+++ lib/server.rb
@@ -5,6 +5,10 @@ require 'omniauth-facebook'
 require './helpers/get_post'
 require 'pry'
 require './lib/facebook_connection'
+require './lib/database'
+
+@@fb = FacebookConnection.new
+@@db = Database.new
 
 class Server < Sinatra::Base
   helpers Sinatra::Cookies
@@ -12,8 +16,8 @@ class Server < Sinatra::Base
   use Rack::Session::Pool
   set :protection, :except => :frame_options
 
-  set :views, File.dirname(__FILE__) + "/../views"
-  set :public_folder, File.dirname(__FILE__) + "/../public"
+  set :views, File.dirname(__FILE__) + '/../views'
+  set :public_folder, File.dirname(__FILE__) + '/../public'
 
   OmniAuth.config.on_failure = lambda do |env|
     [302, {'Location' => '/auth/failure', 'Content-Type' => 'text/html'}, []]
@@ -24,56 +28,50 @@ class Server < Sinatra::Base
   end
 
   use OmniAuth::Builder do
-    APP_ID = 1435770329973305
-    APP_SECRET = '027a9b991a6d50d846ac14256e738b65'
-
-    provider :facebook, APP_ID, APP_SECRET, { :scope => 'user_likes, user_photos' }
+    provider :facebook, @@fb.app_id, @@fb.app_secret, { :scope => @@fb.scopes }
   end
 
   get '/' do
-    @articles = []
-    @articles << {:title => 'Getting Started with Heroku', :url => 'https://devcenter.heroku.com/articles/quickstart'}
-    @articles << {:title => 'Deploying Rack-based apps in Heroku', :url => 'http://docs.heroku.com/rack'}
-    @articles << {:title => 'Learn Ruby in twenty minutes', :url => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
-
-    erb :index
-  end
-  post '/' do
-    @articles = []
-    @articles << {:title => 'Getting Started with Heroku', :url => 'https://devcenter.heroku.com/articles/quickstart'}
-    @articles << {:title => 'Deploying Rack-based apps in Heroku', :url => 'http://docs.heroku.com/rack'}
-    @articles << {:title => 'Learn Ruby in twenty minutes', :url => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
-
+    if !session[:logged_in]
+      session[:flash] = 'Please connect to Facebook to use this app'
+    else
+      session[:flash] = ''
+    end
     erb :index
   end
 
   get '/auth/facebook/callback' do
     fb_auth = request.env['omniauth.auth']
-    session['fb_auth'] = fb_auth
-    session['fb_token'] = fb_auth['credentials']['token']
-    session['fb_error'] = nil
-    session['fb'] = FacebookConnection.new(session['fb_token'])
+    session[:logged_in] = true
+    session[:flash] = 'You have connected to Facebook'
+    session[:name] = fb_auth['info']['first_name']
+
+    @@fb.auth(fb_auth['credentials']['token'])
+    @@fb.get_likes.each do |like|
+      @@db.add fb_auth['uid'], like['id'], like['category'], like['name']
+    end
+
+    session[:likes] = @@db.get_all_for(fb_auth['uid'])
     redirect '/'
   end
 
   get '/auth/failure' do
-    clear_session
-    session['fb_error'] = 'Please allow access to your Facebook data...<br />'
+    session[:logged_in] = false
+    session[:flash] = 'Please allow access to your Facebook data...'
     redirect '/'
   end
 
   get '/login' do
     if settings.redirect_uri
-      # we're in FB
       erb :dialog_oauth
     else
-      # we aren't in FB (standalone app)
       redirect '/auth/facebook'
     end
   end
 
   get '/logout' do
-    clear_session
+    session[:logged_in] = false
+    session[:flash] = 'You have disconnected from Facebook'
     redirect '/'
   end
 
@@ -81,24 +79,16 @@ class Server < Sinatra::Base
   # Canvas URL: http://your_app/canvas/
   # Secure Canvas URL: https://your_app:443/canvas/
   post '/canvas/' do
-
-    # User didn't grant us permission in the oauth dialog
     redirect '/auth/failure' if request.params['error'] == 'access_denied'
 
-    # see /login
-    settings.redirect_uri = 'https://apps.facebook.com/faceboku/'
+    settings.redirect_uri = 'https://apps.facebook.com/matchlike/'
 
-    # Canvas apps send the 'code' parameter
-    # We use it to know if we're accessing the app from FB's iFrame
-    # If so, we try to autologin
-    url = request.params['code'] ? "/auth/facebook?signed_request=#{request.params['signed_request']}&state=canvas" : '/login'
+    url = if request.params['code']
+      "/auth/facebook?signed_request=#{request.params['signed_request']}&state=canvas"
+    else
+      '/login'
+    end
     redirect url
-  end
-
-  def clear_session
-    session['fb_auth'] = nil
-    session['fb_token'] = nil
-    session['fb_error'] = nil
   end
 
 end
